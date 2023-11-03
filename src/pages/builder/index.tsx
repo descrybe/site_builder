5bf6fc8f22448afe3760373fc819e3bbfeadb971
descrybe <descrybe91@gmail.com>
2023-11-03 11:50:09 +0800

    added scss constants, fixed header margin on expand

--- src/pages/builder/index.tsx
+++ src/pages/builder/index.tsx
@@ -1,13 +1,21 @@
 import './style.css'
+import cn from 'classnames'
+import { useAppSelector } from '@store/hooks';
 
 import PageHeader from '@pagesComponent/pageHeader';
 import BuilderPageContent from '@pagesComponent/builderPageContent';
+import { getIsHeaderVisible } from '@store/layout/selectors';
 
 const BuilderPage = () => {
+  const isHeaderVisible = useAppSelector(getIsHeaderVisible)
+
   return (
-    <div id='builder-page-wrapper'>
+    <div
+      id='builder-page-wrapper'
+      className={cn({ hiddenHeader: !isHeaderVisible })}
+    >
       <PageHeader isPossibleToHide />
-      <BuilderPageContent />      
+      <BuilderPageContent />
     </div>
   )
 }
